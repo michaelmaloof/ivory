237173b687a250a789463abfa2adcbed29e83430
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-04-10 19:44:51 -0300

    Initial Commit

---
+++ Ivory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ivory
+//
+//  Created by Michael Maloof on 4/10/16.
+//  Copyright © 2016 TripTrunk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
