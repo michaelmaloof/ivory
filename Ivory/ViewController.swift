6072b93c441bd0ffb9f5bfb93af0f0f86384b395
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-05-10 13:36:00 -0300

    Documentation and Cleanup

--- Ivory/ViewController.swift
+++ Ivory/ViewController.swift
@@ -11,8 +11,6 @@ import AVFoundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    //IBOutlets are done here
-    
     //Camera Elements
     @IBOutlet weak var cameraPreviewView: UIView!
     @IBOutlet weak var capturePhoto: UIButton!
@@ -26,17 +24,133 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     var previewLayer: AVCaptureVideoPreviewLayer!
     var stillImage: UIImage!
     
+//*********************************
+//Stack Changes
+//*********************************
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        capturedImage.layer.cornerRadius = capturedImage.layer.frame.size.width / 2
-        
         toggleCapture(true)
+        //set image on the bottom left to show photo library on click
+        self.establishPhotoLibraryButton()
+        //set up the camera
+        self.establishCamera()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setCameraBounds()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+//*********************************
+//Mark Camera & Photo Library
+//*********************************
+
+    /**
+     The button to capture an image displayed in the camera was tapped
+     
+     @param sender the capture button
+     */
+    @IBAction func captureButtonWasTapped(sender: UIButton) {
+        let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+        stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+            if (sampleBuffer != nil) {
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                
+                self.stillImage = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                UIImageWriteToSavedPhotosAlbum(self.stillImage, "image:didFinishSavingWithError:contextInfo:", nil, nil)
+                self.capturedImage.image = self.stillImage
+                self.performSegueWithIdentifier("showImage", sender: nil)
+            }
+        })
+    }
+    
+    /**
+    The capture button was pressed again to take another photo (no longer used)
+     
+     @param sender the capture button
+     */
+    @IBAction func didPressTakeAnother(sender: AnyObject) {
+        captureSession.startRunning()
+    }
+    
+    /**
+     Toggle if the app is in a "capture" state (taking a photo)
+     
+     @param state is capturing or not
+     */
+    func toggleCapture(state: Bool) {
+        capturedImage.hidden = state
+        capturePhoto.hidden = state
+        cameraPreviewView.hidden = state
+    }
+    
+    /**
+    Image was tapped, save the photo (no longer used)
+     
+     @param img the image tapped
+     */
+    func imageTapped(img: AnyObject) {
+//        UIImageWriteToSavedPhotosAlbum(capturedImage.image!, nil, nil, nil)
+    }
+    
+    /**
+     Did image save to camera roll properly
+     
+     @param image the image saved
+     @param didFinishSavinWithError the error
+    */
+    func image(image: UIImage, didFinishSavingWithError
+        error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
         
-        //Click Image To Show ImagePicker
-        let imageTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.photoImagePressed(_:)))
-        capturedImage.userInteractionEnabled = true
-        capturedImage.addGestureRecognizer(imageTapGestureRecognizer)
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+
+    /**
+     PhotoLibrary image was tapped, show photo library
+     
+     @param gestureRecognizer the gesture recongizer of the image tapped
+     */
+    func photoImagePressed(gestureRecognizer: UITapGestureRecognizer) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        stillImage = image
+        dismissViewControllerAnimated(true) { 
+            self.performSegueWithIdentifier("showImage", sender: nil)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showImage" {
+            let controller = segue.destinationViewController as! PhotoVC
+            controller.stillImage = stillImage
+        }
+    }
+    
+//*********************************
+//Mark Camera & Photo Set-up
+//*********************************
+    
+    func establishCamera(){
         
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSessionPresetPhoto
@@ -71,84 +185,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 toggleCapture(false)
             }
         }
-        
+
     }
-        
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    
+    /**
+     Implement the user's photo library and make it selectable
+     */
+    func establishPhotoLibraryButton(){
+        //round the images of the capturedImage button
+        capturedImage.layer.cornerRadius = capturedImage.layer.frame.size.width / 2
+        //set gesture recognizer to captureImage
+        let imageTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.photoImagePressed(_:)))
+        capturedImage.userInteractionEnabled = true
+        capturedImage.addGestureRecognizer(imageTapGestureRecognizer)
+    }
+    
+    /**
+    Set the camera's bounds (how its displayed on the screen)
+     */
+    func setCameraBounds(){
         let cameraBounds = cameraPreviewView.bounds
         previewLayer.frame = cameraBounds
     }
     
-    //IBActions are done here
-    @IBAction func captureButtonWasTapped(sender: UIButton) {
-        let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo)
-        videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
-        stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
-            if (sampleBuffer != nil) {
-                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                let dataProvider = CGDataProviderCreateWithCFData(imageData)
-                let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
-                
-                self.stillImage = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
-                UIImageWriteToSavedPhotosAlbum(self.stillImage, "image:didFinishSavingWithError:contextInfo:", nil, nil)
-                self.capturedImage.image = self.stillImage
-                self.performSegueWithIdentifier("showImage", sender: nil)
-            }
-        })
-    }
-    
-    @IBAction func didPressTakeAnother(sender: AnyObject) {
-        captureSession.startRunning()
-    }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
-    func toggleCapture(state: Bool) {
-        capturedImage.hidden = state
-        capturePhoto.hidden = state
-        cameraPreviewView.hidden = state
-    }
-    
-    func imageTapped(img: AnyObject) {
-        UIImageWriteToSavedPhotosAlbum(capturedImage.image!, nil, nil, nil)
-    }
-    
-    func image(image: UIImage, didFinishSavingWithError
-        error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
-        
-        if error != nil {
-            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: UIAlertControllerStyle.Alert)
-            
-            let cancelAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
-            
-            alert.addAction(cancelAction)
-            
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
-    }
-
-    func photoImagePressed(gestureRecognizer: UITapGestureRecognizer) {
-        let controller = UIImagePickerController()
-        controller.delegate = self
-        presentViewController(controller, animated: true, completion: nil)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        stillImage = image
-        dismissViewControllerAnimated(true) { 
-            self.performSegueWithIdentifier("showImage", sender: nil)
-        }
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showImage" {
-            let controller = segue.destinationViewController as! PhotoVC
-            controller.stillImage = stillImage
-        }
-    }
-
 }
-
