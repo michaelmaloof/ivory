c828cfd9117b88a3c24d8f86e7d3ac9a5b8a7c5e
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-05-10 21:50:30 -0300

    Workspace Added

---
+++ Pods/Target Support Files/Pods-Ivory/Pods-Ivory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ivory : NSObject
+@end
+@implementation PodsDummy_Pods_Ivory
+@end
---
+++ Pods/Target Support Files/Pods-IvoryUITests/Pods-IvoryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IvoryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IvoryUITests
+@end
